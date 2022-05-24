f28584b9f7c82ab1be46130a27917aaee2ccb3ec
Testacc991 <70212365+Testacc991@users.noreply.github.com>
2022-05-24 16:17:33 +0300

    added proj

---
+++ Vodospad/build/default/_generated_cd4b152bd4cbf8b2.go
@@ -0,0 +1,31 @@
+components {
+  id: "factoryscript"
+  component: "/main/factoryscript.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/objects/logo.go\"\nload_dynamically: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
